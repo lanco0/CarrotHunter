79b273010679adf72a2a5f4f356f5d7b2d081c9d
Hakkı Tumar <77984587+lanco0@users.noreply.github.com>
2023-10-11 17:15:56 +0300

    Update

--- Carrot Hunter/Assets/Scripts/Player.cs
+++ Carrot Hunter/Assets/Scripts/Player.cs
@@ -8,7 +8,9 @@ public class Player : MonoBehaviour
     public float moveSpeed;
 
     float hInput, vInput;
-
+    int score = 0;
+    public GameObject winText;
+    public int winScore;
     
     void Start()
     {
@@ -19,13 +21,32 @@ public class Player : MonoBehaviour
     void Update()
     {
         
-        hInput = joystick.Horizontal * moveSpeed;
-        vInput = joystick.Vertical * moveSpeed;
+       
     }
 
     private void FixedUpdate()
     {
-        
+        hInput = joystick.Horizontal * moveSpeed;
+        vInput = joystick.Vertical * moveSpeed;
+
         transform.Translate(hInput, vInput, 0);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag=="Carrot") 
+        {
+            score++;
+            Destroy(collision.gameObject);
+            
+            if (score >= winScore) 
+            {
+                winText.SetActive(true);
+            }
+
+        }
+
+
+        
     }
 }
